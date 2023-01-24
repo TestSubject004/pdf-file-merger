46b8a5833172da5edbbf7a15bda217de7a295230
Tigmanshu Kumar <49922909+TestSubject004@users.noreply.github.com>
2023-01-24 18:34:53 +0530

    Add files via upload

---
+++ merge.py
@@ -0,0 +1,43 @@
+import os;
+from PyPDF2 import PdfMerger;
+
+
+cwd = os.getcwd()
+
+print("Th Current working directory is: {0}".format(cwd))
+
+dirList = []
+fList = []
+
+
+def getList(list):
+    for filename in os.scandir(cwd):
+        f = os.path.join(cwd, filename)
+        # checking if it is a dir
+        if filename.is_dir():
+            list.append(f)
+
+
+getList(dirList)
+
+for i in dirList:
+    print(i)
+
+for dir in dirList:
+    merger = PdfMerger()
+    cwd = os.chdir(dir)
+    cwd = os.getcwd()
+
+    for filename in os.listdir(cwd):
+        merger.append(filename)
+       
+        print (filename)
+    
+    merger.write("result.pdf")
+    merger.close()
+    print("--------------------------")
+   
+
+
+
+
