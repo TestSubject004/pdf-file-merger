01791528fa1c012fcc7e51c1902aec763f3c089c
Tigmanshu Kumar <49922909+TestSubject004@users.noreply.github.com>
2023-01-24 23:03:41 +0530

    Update merge.py
    refactored code and fixed bugs

--- merge.py
+++ merge.py
@@ -1,42 +1,73 @@
 import os;
-from PyPDF2 import PdfMerger;
+from PyPDF2 import PdfMerger,PdfFileReader, PdfReader;
+from PyPDF2.errors import PdfReadError;
+import pathlib
+
 
 
 cwd = os.getcwd()
 
 print("Th Current working directory is: {0}".format(cwd))
 
-dirList = []
-fList = []
 
 
-def getList(list):
+def procDir():
+    
+    flist=[]
     for filename in os.scandir(cwd):
+        f = os.path.join(cwd, filename)
+      
+        if filename.is_file() and filename.name.endswith('.pdf'):
+            
+            
+            flist.append(filename.name)
+            print(filename.name)
+                
+            
+    if flist:
+        merger = PdfMerger()
+        for lis in flist:
+            merger.append(lis)        
+        merger.write(f"{os.path.basename(cwd)}.pdf")
+        merger.close()
+    print("--------------------------")
+    return getList()
+
+
+def getList():
+    dirList=[]
+    for filename in os.scandir(cwd):
+        
         f = os.path.join(cwd, filename)
         # checking if it is a dir
         if filename.is_dir():
-            list.append(f)
+            dirList.append(f)
+    if dirList:
+        return dirList
+    else:
+        return None
 
 
-getList(dirList)
 
-for i in dirList:
-    print(i)
 
-for dir in dirList:
-    merger = PdfMerger()
-    cwd = os.chdir(dir)
-    cwd = os.getcwd()
-
-    for filename in os.listdir(cwd):
-        merger.append(filename)
-       
-        print (filename)
+def exec():
+    global cwd
+    abc = procDir()
     
-    merger.write(f"{os.path.basename(dir)}.pdf")
-    merger.close()
-    print("--------------------------")
-   
+    if abc:
+        for dir in abc:
+            os.chdir(dir)
+            cwd = os.getcwd()
+            exec()
+    else:
+        return
+        
+            
+    
+    
+exec ()
+
+
 
 
 
